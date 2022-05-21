3216a96ea1c2c3b65c17e2d5d8d5360ea7c20737
Stan <s2000ProMax@gmail.com>
2022-05-21 20:32:55 +0300

    add: countries and users api

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from './components/ui/navBar';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import Main from './layouts/main';
 import Error404 from './components/common/page/error404';
 import Login from './layouts/login';
+import api from './api';
+
 const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [clients, setClients] = useState([]);
+  const [customers, setCustomers] = useState([]);
+  useEffect(() => {
+    api.countries.fetchCountries().then(countriesData => setCountries(countriesData.data));
+    api.clients.fetchAll().then(clientsData => setClients(clientsData));
+    api.customers.fetchAll().then(customersData => setCustomers(customersData));
+  }, []);
+  console.log(countries);
+  console.log(clients);
+  console.log(customers);
+
   return (
     <>
       <NavBar />
--- src/app/layouts/main.jsx
+++ src/app/layouts/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Main = () => {
+
   return (
     <h1>Main page</h1>
   );
